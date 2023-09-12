cf2455f24b6327275f1599fe1fe3d21c63ff4dcd
Bryce Koch <113659870+BKoch74@users.noreply.github.com>
2023-09-12 08:38:39 -0500

    Update mascot_test.go

--- Assignments/P01/mascot/mascot_test.go
+++ Assignments/P01/mascot/mascot_test.go
@@ -1,3 +1,6 @@
+// Bryce Koch
+// 4143 PLC
+
 package mascot_test
 
 import (
@@ -6,6 +9,8 @@ import (
 	"github.com/BKoch74/4143-PLC/mascot"
 )
 
+// Runs a test to verify that the BestMascot is a Lion. If not,
+// outputs the error message to the console.
 func TestMascot(t *testing.T) {
 	if mascot.BestMascot() != "Lion" {
 		t.Fatal("Wrong mascot!")
