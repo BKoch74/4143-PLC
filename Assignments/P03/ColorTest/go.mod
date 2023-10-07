892d61bbb1b8073891cdb966b1442cfd01226386
Bryce <brycekoch179@gmail.com>
2023-10-06 23:00:09 -0500

    commit

--- Assignments/P03/ColorTest/go.mod
+++ Assignments/P03/ColorTest/go.mod
@@ -5,7 +5,7 @@ go 1.21.1
 require github.com/BKoch74/Image_Ascii_Art v0.0.0-20231007031328-8e29fe26ff67
 
 require (
-	github.com/fogleman/gg v1.3.0 // indirect
+    	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	golang.org/x/image v0.13.0 // indirect
 )
--- Assignments/P03/ColorTest/main.go
+++ Assignments/P03/ColorTest/main.go
@@ -1,3 +1,28 @@
+/*************************************************************************************************
+*
+*  Name				 Bryce Koch
+*  Label:            P03
+*  Title:            Program 3 - Image_Ascii_Art
+*  Course:           CMPS 4143-PLC
+*  Semester:         Fall 2023
+**************************************************************************************************
+*  Description:
+*        This program implements four image programs that were created and uploaded to
+*		 GitHub. The programs are being pulled in from the GitHub repository and being used
+*		 in this code. The four programs download an image from an URL, gather the pixel
+*		 color information of an image, change the color to gray, and add text to an image.
+**************************************************************************************************
+*  Usage:
+*        - Run go get in the terminal to pull the repository.
+*		 - Run go run main.go to compile the program.
+**************************************************************************************************
+*  Files:
+*        main.go   			 :  main driver
+*        go.mod	   			 :  defines the program's module
+*        go.sum	   			 :  Downloads and holds imported files.
+*
+*************************************************************************************************/
+
 package main
 
 import (
